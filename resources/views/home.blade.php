11e8c7e1a263cbcde635e6a8f47c7bc7162fd311
Hassan <h-@outlook.in>
2019-07-26 07:28:13 +0300

    fix bugs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,26 @@
 @section('content')
 <div class="container text-center" style="top:0; position:absolute; width:100%;">
     <div class="row">
-        <div class="col p-4 bg-warning"><i class="fas fa-car fa-2x text-white"></i> <h2 class="d-inline text-white pl-4">{{$total['Driver']}}</h2></div>
-        <div class="col p-4 bg-secondary"><i class="fas fa-shopping-cart fa-2x text-white"></i><h2 class="d-inline text-white pl-4">{{$total['Shop']}}</h2></div>
+        <div class="col p-4 bg-warning">
+            <i class="fas fa-car fa-2x text-white"></i> <h2 class="d-inline text-white pl-4">{{$total['Driver']}}</h2>
+        </div>
+        <div class="col p-4 bg-secondary">
+            <i class="fas fa-shopping-cart fa-2x text-white"></i><h2 class="d-inline text-white pl-4">{{$total['Shop']}}</h2>
+        </div>
     </div>
     <div class="row">
-        <div class="col p-2 text-center bg-info"><i class="fas fa-equals fa-2x text-white d-inline"></i><h2 class="d-inline text-white pl-4">{{$total['all']}}</h2></div>
+        <div class="col p-2 text-center bg-info">
+            <i class="fas fa-equals fa-2x text-white d-inline"></i><h2 class="d-inline text-white pl-4">{{$total['all']}}</h2>
+        </div>
     </div>
 </div>
     <table class="table w-100 text-center" style="position:absolute;top:140px;">
         <thead>
             <tr>
-            <th scope="col">#</th>
-            <th scope="col">#</th>
-            <th scope="col">Amount</th>
-            <th scope="col">Date</th>
+                <th scope="col">#</th>
+                <th scope="col">#</th>
+                <th scope="col">Amount</th>
+                <th scope="col">Date</th>
             </tr>
         </thead>
         <tbody>
